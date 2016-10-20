76ee7a0d1d7551bc8bbd5b487696ffc1c8b72180
Jonathan <infixz@outlook.com>
2016-10-20 17:43:52 +0800

    continue

--- blog/app.py
+++ blog/app.py
@@ -2,17 +2,18 @@
 """
 app defined here
 """
-
+from functools import wraps
 from flask import Flask
 from flask import request, session, make_response, redirect, abort, url_for, flash
 from flask import render_template
 from flask.ext.script import Manager, Shell
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+
 
 #from models import Role, User
 from local_settings import MySQL_URI, SECRET_KEY
@@ -21,17 +22,54 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = MySQL_URI
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = SECRET_KEY
 
 manager = Manager(app)
 db = SQLAlchemy(app)
+
+# represent style
 bootstrap = Bootstrap(app)
+
+
+def ops_record_broken(module_name='camel'):
+    def deco(func):
+        print 'deco'
+        print module_name
+        print func.__name__
+        # print request.values
+        # print request.url
+
+        def warpped(*argu, **argv):
+            return func(*argu, **argv)
+        return warpped
+    return deco
+
+
+def ops_record(module_name='camel'):
+    """closure return a deco"""
+    def deco(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            resp = func(*args, **kwargs) 
+            if request.environ['REQUEST_METHOD'] == 'GET':
+                return resp
+            print ':module_name:', module_name
+            print ':func.__name__:', func.__name__
+            print ':requ_env:', request.environ
+            print ':requ.values:', request.values
+            print ':form2dict:', request.form.to_dict()
+            return resp
+        return wrapper
+    return deco
+
 
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role')
+
     def __repr__(self):
         return '<Role %r>' % self.name
 
@@ -41,6 +79,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -53,11 +92,14 @@ class NameForm(Form):
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
+    print ':user_agent:', user_agent
     return render_template('index.html')
 
 
-@app.route('/user/', methods=['GET', 'POST'])
-@app.route('/user/<name>')
+@app.route('/user/', methods=['GET', 'POST', 'PUT'])
+# @app.route('/user/<name>')
+# @ops_record('user_part')
+@ops_record('user')
 def user_index():
     name = None
     form = NameForm()
@@ -72,9 +114,11 @@ def user_index():
 # return render_template('user.html', name=session.get('name','Not in
 # session'))
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
--- blog/views.py
+++ blog/views.py
@@ -14,12 +14,85 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
 
+class OpsRecord(object):
+    """This class will create a usr operations record
+    decorator which will save user's ip,req method,url,
+    resp_status"""
+    def __init__(self, ):
+        print "inside myDecorator.__init__()"
+        self.fn = fn
+ 
+    def __call__(self, func):
+        self.fn()
+        print "inside myDecorator.__call__()"
+
+
+def ops_record(module_name):
+    def deco(func):
+        print 'deco apart'
+        print module_name
+        print func.__name__
+        print request.values
+        print request.url
+        def warpped(*argu, **argv):
+            func(*argu, **argv)
+        return warpped
+    return deco
+
+def deco(func):
+    print 'deco apart'
+    def warpped(*argu, **argv):
+        func(*argu, **argv)
+    return warpped
+
+def add_html_tag(tag, *argu, **kargv):
+    def deco(func):
+        css_class = " class='{0}'".format(kargv['css_class'] if "css_class" in kargv else "")
+        def warpped(*argu, **argv):
+            return css_class + func(*argu, **argv) + "</"+tag+">"
+        return warpped
+    return deco
+
+    
+class Retry(object):
+    """This class will create a retry decorator.
+    Using is_valid to judge if wrapped function failed.
+    Retry if wrapped function failed.
+    Retry at most MAX_TRIES times.
+    """
+    MAX_TRIES = 3
+
+    def __init__(self, is_valid=id, max_tries=3):
+        self.is_valid = is_valid
+        self.MAX_TRIES = max_tries
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def retried_func(*args, **kwargs):
+            resp = None
+            tries = 0
+            while tries < self.MAX_TRIES:
+                try:
+                    resp = func(*args, **kwargs)
+                except Exception:
+                    continue
+                if self.is_valid(resp):
+                    break
+                tries += 1
+            return resp
+        return retried_func
+
+
+retry = Retry()
+
+@deco
 @app.route('/')
+
 def index():
     user_agent = request.headers.get('User-Agent')
     return render_template('index.html')
 
-
+@ops_record('user_part')
 @app.route('/user/', methods=['GET','POST'])
 def user_index():
     name = None
